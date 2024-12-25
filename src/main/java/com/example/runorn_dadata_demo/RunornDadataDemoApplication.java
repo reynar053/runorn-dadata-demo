b3af3d56a6645ea2ce0eb40d0d592ac8bd6d9a6c
fox <fox>
2024-12-25 16:42:19 +0300

    Initial commit

---
+++ src/main/java/com/example/runorn_dadata_demo/RunornDadataDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.runorn_dadata_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunornDadataDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunornDadataDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/runorn_dadata_demo/RunornDadataDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.runorn_dadata_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunornDadataDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
