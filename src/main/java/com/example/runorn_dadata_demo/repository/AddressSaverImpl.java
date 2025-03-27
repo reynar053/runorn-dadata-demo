fbdd2de5c9cbcec63e635ab36c7c2aea6ce0696e
Runorn <runorntopyt@gmail.com>
2025-03-27 21:40:46 +0300

    поменял логику, теперь сохраняем не в лист, а в мапу и даем каждому id, так же сделал метод получения элемента мапы по id

--- src/main/java/com/example/runorn_dadata_demo/repository/AddressSaverImpl.java
+++ src/main/java/com/example/runorn_dadata_demo/repository/AddressSaverImpl.java
@@ -6,18 +6,28 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Data
 @Component
 @Slf4j
 public class AddressSaverImpl implements AddressSaver {
 
-  private final List<AddressResponse> addressesList = new ArrayList<>();
+  private final Map<Integer, AddressResponse> addressMap = new HashMap<>();
+  private final AtomicInteger idGenerator = new AtomicInteger();
 
   @Override
   public void saveAddress(List<AddressResponse> addresses) {
-   addressesList.add(addresses.get(0));
-    log.info("{} addresses saved", addressesList);
+   AddressResponse address = addresses.get(0);
+   int id = idGenerator.getAndIncrement();
+   log.info("{} addresses saved", address);
+   addressMap.put(id, address);
+  }
+
+  public AddressResponse getAddressById(int id) {
+    return addressMap.get(id);
   }
 }
