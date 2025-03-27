f05500e31b73bff9adfe19b76cf6c1c05959cac8
Runorn <runorntopyt@gmail.com>
2025-03-27 21:43:44 +0300

    удалил лишние импорты

--- src/main/java/com/example/runorn_dadata_demo/repository/AddressSaverImpl.java
+++ src/main/java/com/example/runorn_dadata_demo/repository/AddressSaverImpl.java
@@ -5,7 +5,7 @@ import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
