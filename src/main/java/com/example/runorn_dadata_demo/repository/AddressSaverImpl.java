734f77301e3b458fb1c14669ba7ede7c9043e344
Runorn <runorntopyt@gmail.com>
2025-03-28 18:16:32 +0300

    вместо AtomicInteger сделал просто int, дал более коретное название методу, удалил ненужную аннотацию @Data

--- src/main/java/com/example/runorn_dadata_demo/repository/AddressSaverImpl.java
+++ src/main/java/com/example/runorn_dadata_demo/repository/AddressSaverImpl.java
@@ -1,32 +1,30 @@
 package com.example.runorn_dadata_demo.repository;
 
 import com.example.runorn_dadata_demo.model.AddressResponse;
-import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
 
-@Data
+
 @Component
 @Slf4j
 public class AddressSaverImpl implements AddressSaver {
-
   private final Map<Integer, AddressResponse> addressMap = new HashMap<>();
-  private final AtomicInteger idGenerator = new AtomicInteger();
+  private int id = 0;
+
 
   @Override
-  public void saveAddress(List<AddressResponse> addresses) {
+  public void saveFirstAddress(List<AddressResponse> addresses) {
    AddressResponse address = addresses.get(0);
-   int id = idGenerator.getAndIncrement();
+   id++;
    log.info("{} addresses saved", address);
    addressMap.put(id, address);
   }
 
+@Override
   public AddressResponse getAddressById(int id) {
     return addressMap.get(id);
   }
